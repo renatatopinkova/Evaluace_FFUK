a0fbb4f03b61c8b03a7cfa88155c3223ded25e95
renatatopinkova <r.topinkova@gmail.com>
2022-07-08 21:11:54 +0100

    working in AV code & DRY a bit

--- 2_evaluace_scraping.R
+++ 2_evaluace_scraping.R
@@ -33,6 +33,14 @@ url <- "https://hodnoceni.ff.cuni.cz/results/evaluation/summer-term-20212022/cou
 # navigate to evaluace
 browser$navigate(url)
 
+
+# Start scraping ----------------------------------------------------------
+
+
+
+## Get basic course info ---------------------------------------------------
+
+
 # get course name
 course_name <- browser$findElement("class", "ev--page-title")$getElementText()
 
@@ -49,59 +57,95 @@ teacher_box <- browser$findElements("class", "teacher-boxs") %>%
   flatten()
 
 # get average course rating
-avg_course <- browser$findElement("class", "course-average-rating")$getElementText() %>%
-  # extract number w/ decimals
-  str_extract("\\d+\\.?\\d*")
+avg_course <- browser$findElement("class", "course-average-rating")$getElementText()
+
+
+# get N of ratings and N of students
+n_rating <- browser$findElements("class", "ta-r") %>%
+  map(~.x$getElementText()) 
+
+
+
+## Get particular ratings --------------------------------------------------
+
+
+
+questions_wording <- c("S uvážením všeho, co jste do této chvíle vyplnil/a, zhodnote, prosíme, pøedmìt celkovì.",
+                       "Byl pro Vás pøedmìt pøínosný?",
+                       "Pøekrýval se podle Vás obsah výuky v tomto pøedmìtu zbyteènì s obsahem výuky v jiném pøedmìtu?",
+                       "Jaká byla podle Vás nároènost pøedmìtu v porovnání s ostatními pøedmìty?",
+                       "Zhodnote, prosíme, pedagogické pùsobení vyuèujícího.")
+
 
 # get each question, n of answers, and average answer
 rating <- browser$findElements("class", "results-question-item") %>% 
   map(~.x$getElementText()) %>%
   map(~str_split(.x, pattern = "\\n")) %>% 
-  flatten() 
+  flatten() %>% 
+  map(~tibble(var = .[1],
+                      resp = .[2],
+                      val  = .[3])) %>%
+  bind_rows() %>%
+  filter(!str_detect(resp, 'Komentáøù')) %>% 
+  mutate_at(c("resp", "val"), str_extract, "\\d+\\.?\\d*") %>%
+  mutate(var = case_when(var == questions_wording[1] ~ "total",
+                         var == questions_wording[2] ~ "useful",
+                         var == questions_wording[3] ~ "overlap",
+                         var == questions_wording[4] ~ "difficulty",
+                         var == questions_wording[5] ~ "teacher"))
 
 
 
-# get N of ratings and N of students
-n_rating <- browser$findElements("class", "ta-r") %>%
-  map(~.x$getElementText())
 
 
-
-# Get the comments --------------------------------------------------------
+## Get the comments --------------------------------------------------------
 
 
 # show comments for positives
 browser$findElement("class", "blue-hotfix-button")$clickElement()
 
+
 # scrape content
 positive <- browser$findElements("css", ".ev--dark-text p") %>%
-  map(~.x$getElementText())
+  map(~.x$getElementText()) 
 
 # toggle comments for positive - hide them
 browser$findElement("class", "blue-hotfix-button")$clickElement()
 
 
 # show comments to negative
-browser$findElement("css selector", ".results-question-item+ .results-question-item .clickable")$clickElement()
+browser$findElement("css", ".results-question-item+ .results-question-item .clickable")$clickElement()
 
 # scrape content
 negative <- browser$findElements("css", ".ev--dark-text p") %>%
-  map(~.x$getElementText())
+  map(~.x$getElementText()) 
 
 
 # Putting it together ??  -------------------------------------------------
 
-comments <- list(negative = flatten(negative), 
-                 positive = flatten(positive))
 
+# get course rating rundown
+tibble(course_name = course_name, 
+       semester = semester,
+       avg = avg_course, 
+       rated = n_rating[[1]], 
+       n_students = n_rating[[2]], 
+       url = url, 
+       var = rating$var,
+       resp = rating$resp,
+       val = rating$val) %>%
+  unnest(course_name) %>% 
+  mutate_at(., c("avg", "rated", "n_students", "resp", "val"), ~as.numeric(str_extract(., "\\d+\\.?\\d*")))
+  
 
-
-tibble(course_name = course_name, avg = avg_course, rated = n_rating[[1]], n_students = n_rating[[2]], url = url) %>%
-  unnest(everything())
-
-
+# get teacher stats 
 tibble(course_name = course_name, url = url, teacher = teacher_box) %>%
   unnest(everything()) %>% 
   mutate(teacher_rating = as.numeric(str_extract(teacher, "\\d+\\.?\\d*")),
-         teacher = str_trim(str_remove(teacher, "\\d+\\.?\\d*%")))
-  
+         teacher = str_trim(str_remove(teacher, "\\d+\\.?\\d*%"))) %>% 
+  bind_cols(., rating[rating$var == "teacher", ])
+
+
+## TODO: would be nice to have comments as nested column in teacher stats
+comments <- list(negative = flatten(negative), 
+                 positive = flatten(positive))
